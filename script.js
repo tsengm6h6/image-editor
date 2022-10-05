c914a3121495449f7b7547363b166e8093b807b8
yutzu <tsengm6h6@gmail.com>
2022-10-05 21:34:47 +0800

    feat: add filter & slider function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const fileInput = document.querySelector('.file-input')
 const chooseImgBtn = document.querySelector('.choose-image')
 const previewImg = document.querySelector('.image-preview img')
+const filterOptions = document.querySelectorAll('.filter .options .button')
+const filterName = document.querySelector('.slider .slider-info .name')
+const filterValue = document.querySelector('.slider .slider-info .value')
+const sliderInput = document.querySelector('.slider input')
 
 const loadImage = (evt) => {
   const file = evt.target.files[0]
@@ -13,3 +17,55 @@ const loadImage = (evt) => {
 
 fileInput.addEventListener('change', loadImage)
 chooseImgBtn.addEventListener('click', () => fileInput.click())
+
+const DEFALTFILTER = {
+  brightness: 100,
+  saturation: 100,
+  inversion: 0,
+  grayscale: 0
+}
+
+let currentFilter = {
+  brightness: 100,
+  saturation: 100,
+  inversion: 0,
+  grayscale: 0
+}
+
+const sliderMax = {
+  brightness: 200,
+  saturation: 200,
+  inversion: 100,
+  grayscale: 100
+}
+
+const applyChange = () => {
+  previewImg.style.filter = `
+    brightness(${currentFilter.brightness}%)
+    saturate(${currentFilter.saturation}%)
+    invert(${currentFilter.inversion}%)
+    grayscale(${currentFilter.grayscale}%)
+  `
+}
+
+// filter
+const setCurrentFilter = (option) => {
+  document.querySelector('.options .active').classList.remove('active')
+    option.classList.add('active')
+    filterName.innerText = option.innerText
+    filterValue.innerText = `${currentFilter[option.id]}%`
+    sliderInput.max = sliderMax[option.id]
+    sliderInput.value = currentFilter[option.id]
+}
+
+const updateFilter = (evt) => {
+  filterValue.innerText = `${evt.target.value}%`
+  const selectedOption = document.querySelector('.options .active')
+  currentFilter[selectedOption.id] = evt.target.value
+  applyChange()
+}
+
+filterOptions.forEach(option => {
+  option.addEventListener('click', () => setCurrentFilter(option))
+})
+sliderInput.addEventListener('input', updateFilter)
