f2561d1e3605f790b666decaa16c0932c1898061
yutzu <tsengm6h6@gmail.com>
2022-10-05 21:41:52 +0800

    feat: add save image

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const filterValue = document.querySelector('.slider .slider-info .value')
 const sliderInput = document.querySelector('.slider input')
 const transformOptions = document.querySelectorAll('.rotate .options .button')
 const resetBtn = document.querySelector('.button.reset')
+const saveBtn = document.querySelector('.button.save')
 
 const loadImage = (evt) => {
   const file = evt.target.files[0]
@@ -54,13 +55,17 @@ const sliderMax = {
   grayscale: 100
 }
 
-const applyChange = () => {
-  previewImg.style.filter = `
+const getFilter = () => {
+  return `
     brightness(${currentFilter.brightness}%)
     saturate(${currentFilter.saturation}%)
     invert(${currentFilter.inversion}%)
     grayscale(${currentFilter.grayscale}%)
   `
+}
+
+const applyChange = () => {
+  previewImg.style.filter = getFilter()
   previewImg.style.transform = `
     rotate(${currentTransform.rotate}deg)
     scale(${currentTransform.flipX}, ${currentTransform.flipY})
@@ -113,3 +118,34 @@ const reset = () => {
 }
 
 resetBtn.addEventListener('click', reset)
+
+const save = () => {
+  saveBtn.innerText = 'SAVING IMAGE...'
+  saveBtn.classList.add('disabled')
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d') // 取得繪製環境
+  canvas.width = previewImg.naturalWidth // set canvas width to actual image width
+  canvas.height = previewImg.naturalHeight // set canvas height to actual image height
+  // drawImage(image, x, y, width, height)
+  ctx.filter = getFilter()
+  // 處理 transform
+  const { rotate, flipX, flipY } = currentTransform
+  ctx.translate(canvas.width / 2, canvas.height /2) // 找到圖片中心點
+  // 先 rotate 再 scale，否則旋轉角度會不正確
+  if (rotate !== 0) {
+    ctx.rotate(rotate * Math.PI / 180)
+  }
+  ctx.scale(flipX, flipY) // 縮放值若是負數，會造成座標軸鏡射 -> 笛卡爾座標系
+  ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height) // 拉回左上角（=對齊中心點，鏡射才不會跑掉）
+  
+  const link = document.createElement('a')
+  link.download = 'image.jpg'
+  link.href = canvas.toDataURL()
+  link.click()
+  setTimeout(() => {
+    saveBtn.innerText = 'SAVE IMAGE'
+    saveBtn.classList.remove('disabled')
+  }, 3000);
+}
+
+saveBtn.addEventListener('click', save)
